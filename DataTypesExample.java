7ab1676ef0c5811245bb08e982df2d9b8cf97d77
Sumanth Reddy <99172257+sumanth786@users.noreply.github.com>
2022-02-11 01:37:43 -0600

    Add files via upload

---
+++ DataTypesExample.java
@@ -0,0 +1,14 @@
+public class DataTypesExample{
+	public static void main(String args[]){
+		int StudentAge = 23;
+		double StudentGPA = 3.2;
+		char StudentFirstInitial = 'S';
+		char StudentLastInitial = 'R';
+		boolean hasPrerequisites = true;
+		System.out.println(StudentAge);
+		System.out.println(StudentGPA);
+		System.out.println(StudentFirstInitial);
+		System.out.println(StudentLastInitial);
+		System.out.println(hasPrerequisites);
+	}
+}
