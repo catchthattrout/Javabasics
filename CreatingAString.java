00a40a24860c11865105f0bccf75f5ffbf8923f2
Sumanth Reddy <99172257+sumanth786@users.noreply.github.com>
2022-02-11 01:57:29 -0600

    Add files via upload

---
+++ CreatingAString.java
@@ -0,0 +1,10 @@
+public class CreatingAString{
+	public static void main(String args[]){
+
+		String StudentFirstName = "Alisha";
+	    String StudentLastName = "McDonald";
+	    System.out.println(StudentFirstName);
+	    System.out.println(StudentLastName);
+
+	}
+}
