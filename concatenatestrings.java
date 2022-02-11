9d560ed4a247df3df0edaf1fe7e95038ca9e6a51
Sumanth Reddy <99172257+sumanth786@users.noreply.github.com>
2022-02-11 02:28:28 -0600

    Add files via upload

---
+++ concatenatestrings.java
@@ -0,0 +1,8 @@
+public class concatenatestrings{
+	public static void main(String args[]){
+		String StudentFirstName = "Jennifer";
+		String StudentLastName = "Cooper";
+		int StudentAge = 23;
+		System.out.println(StudentFirstName + " " + StudentLastName + " is " + StudentAge); 
+	}
+}
