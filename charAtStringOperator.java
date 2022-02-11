9b47b28ca31062d7c158f573f5a3d6b8570ea92d
Sumanth Reddy <99172257+sumanth786@users.noreply.github.com>
2022-02-11 02:19:02 -0600

    Add files via upload

---
+++ charAtStringOperator.java
@@ -0,0 +1,13 @@
+public class charAtStringOperator{
+	public static void main(String args[]){
+
+	    String StudentFirstName = "Richard";
+	    String StudentLastName = "Kennedy";
+	    System.out.println(StudentFirstName);
+	    System.out.println(StudentLastName);
+	    char StudentFirstNameInitial = StudentFirstName.charAt(0);
+	    char StudentLastNameInitial = StudentLastName.charAt(0);
+	    System.out.println(StudentFirstNameInitial);
+	    System.out.println(StudentLastNameInitial);
+	}
+}
